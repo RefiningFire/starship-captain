00d0774a57ce25b00bda87690bef647e77982961
RefiningFire <93163433+RefiningFire@users.noreply.github.com>
2021-11-10 20:17:08 -0800

    syntax correction

--- tactical_missions.py
+++ tactical_missions.py
@@ -6,6 +6,6 @@ def duel_target_ship(target_name, target_hull, target_armor, target_shields, tar
     print("Target Power: " + str(target_power))
     print("Target Systems: " + str(target_systems))
     locked_in_combat = True
-    while locked_in_combat = True:
+    while locked_in_combat == True:
         if target_hull > 0 and hull > 0:
             print("Both Hull and Target Hull > 0")
