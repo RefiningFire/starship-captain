17241d76bc0bee3f4d41f60546a5321837a36d40
RefiningFire <93163433+RefiningFire@users.noreply.github.com>
2021-12-08 21:19:18 -0800

    working with sprites

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # starship-captain
 A python learning project, in the form of a text adventure game about interstellar war
+
+#Terran Ships
+Interceptor 32 x 24
+Fighter 40 x 32
+Heavy Fighter 48 x 35
+
+Courier 48 x 38
+Transport 56 x 34
+Freighter 80 x 50
+
+Destroyer 96 x 60
+Warship 128 x 119
+Cruiser 152 x 132
+Carrier 160 x 116
+Dreadnaught 200 x 141
--- main.py
+++ main.py
@@ -19,7 +19,24 @@ background = pygame.Surface((screen_size_x, screen_size_y))
 background.fill(pygame.Color('#000000'))
 
 manager = pygame_gui.UIManager((screen_size_x, screen_size_y), 'theme.json')
-manager_2 = pygame_gui.UIManager((screen_size_x, screen_size_y), 'button.json')
+
+
+class Fighter(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.sheet = pygame.image.load('sprites/meowx/Terran/Destroyer/96 X 60.png').convert_alpha()
+
+    def image_at(self, rectangle):
+        rect = pygame.Rect(rectangle)
+        image = pygame.Surface(rect.size).convert()
+        image.blit(self.sheet, (0, 0), rect)
+
+
+all_sprites = pygame.sprite.Group()
+image_coor = (50,50,100,100)
+player = Fighter.image_at(image_coor)
+all_sprites.add(player)
 
 hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(
     ((screen_size_x // 2) - 100, 0), (button_size_x, button_size_y)),
@@ -51,6 +68,9 @@ is_running = True
 
 while is_running:
     time_delta = clock.tick(60)/1000.0
+
+    all_sprites.draw(background)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
