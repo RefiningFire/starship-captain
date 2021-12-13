d3a462d47c37e72399126841948f6ff021868542
RefiningFire <93163433+RefiningFire@users.noreply.github.com>
2021-12-12 16:40:44 -0800

    building out spritesheet class to handle animation

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ pygame.init()
 pygame.display.set_caption('Quick Start')
 window_surface = pygame.display.set_mode((screen_size_x, screen_size_y))
 
+starfield = pygame.image.load('sprites/Starfield.png')
 background = pygame.Surface((screen_size_x, screen_size_y))
 background.fill(pygame.Color('#000000'))
 
@@ -52,48 +53,13 @@ text_window = pygame_gui.elements.UILabel(relative_rect=pygame.Rect(
 
 
 
-my_spritesheet = Spritesheet('sprites/meowx/Terran/Fighter/40 X 32.png')
+my_spritesheet = Spritesheet('fighter','sprites/meowx/Terran/Fighter/40 X 32.png')
+fighter = my_spritesheet.frame_sheet()
 
 fighter_loc_x = 200
 fighter_loc_y = 200
 
-fighter = [my_spritesheet.parse_sprite('fighter1.png'),                 my_spritesheet.parse_sprite('fighter2.png'),    my_spritesheet.parse_sprite('fighter3.png'), my_spritesheet.parse_sprite('fighter4.png'), my_spritesheet.parse_sprite('fighter5.png'), my_spritesheet.parse_sprite('fighter6.png'),
-my_spritesheet.parse_sprite('fighter7.png'),
-my_spritesheet.parse_sprite('fighter8.png'),
-my_spritesheet.parse_sprite('fighter9.png'),
-
-my_spritesheet.parse_sprite('fighter10.png'),
-my_spritesheet.parse_sprite('fighter11.png'),
-my_spritesheet.parse_sprite('fighter12.png'),
-my_spritesheet.parse_sprite('fighter13.png'),
-my_spritesheet.parse_sprite('fighter14.png'),
-my_spritesheet.parse_sprite('fighter15.png'),
-my_spritesheet.parse_sprite('fighter16.png'),
-my_spritesheet.parse_sprite('fighter17.png'),
-my_spritesheet.parse_sprite('fighter18.png'),
-
-my_spritesheet.parse_sprite('fighter19.png'),
-my_spritesheet.parse_sprite('fighter20.png'),
-my_spritesheet.parse_sprite('fighter21.png'),
-my_spritesheet.parse_sprite('fighter22.png'),
-my_spritesheet.parse_sprite('fighter23.png'),
-my_spritesheet.parse_sprite('fighter24.png'),
-my_spritesheet.parse_sprite('fighter25.png'),
-my_spritesheet.parse_sprite('fighter26.png'),
-my_spritesheet.parse_sprite('fighter27.png'),
-
-my_spritesheet.parse_sprite('fighter28.png'),
-my_spritesheet.parse_sprite('fighter29.png'),
-my_spritesheet.parse_sprite('fighter30.png'),
-my_spritesheet.parse_sprite('fighter31.png'),
-my_spritesheet.parse_sprite('fighter32.png'),
-my_spritesheet.parse_sprite('fighter33.png'),
-my_spritesheet.parse_sprite('fighter34.png'),
-my_spritesheet.parse_sprite('fighter35.png'),
-my_spritesheet.parse_sprite('fighter36.png')]
-
-index = 0
-
+spritesheet_index = 0
 
 clock = pygame.time.Clock()
 is_running = True
@@ -101,26 +67,26 @@ is_running = True
 while is_running:
     time_delta = clock.tick(60)/1000.0
 
+    keys = pygame.key.get_pressed()  #checking pressed keys
+    if keys[pygame.K_RIGHT]:
+        spritesheet_index = (spritesheet_index + 1) % len(fighter)
+    if keys[pygame.K_LEFT]:
+        spritesheet_index = (spritesheet_index - 1) % len(fighter)
+    if keys[pygame.K_UP]:
+        fighter_loc_y -= 3
+    if keys[pygame.K_DOWN]:
+        fighter_loc_y += 3
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                index = (index + 1) % len(fighter)
-            elif event.key == pygame.K_LEFT:
-                index = (index - 1) % len(fighter)
-            if event.key == pygame.K_UP:
-                fighter_loc_x += 1
-            elif event.key == pygame.K_DOWN:
-                fighter_loc_x -= 1
-
 
         if event.type == pygame.USEREVENT:
             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == hello_button:
                     text_window.set_text('Hello World!')
                 elif event.ui_element == subtract_button:
-                    count -= 1s
+                    count -= 1
                     total_button.set_text(f'{count}')
                     text_window.set_text('Subtracting...')
                 elif event.ui_element == add_button:
@@ -134,7 +100,8 @@ while is_running:
 
     manager.update(time_delta)
     background.fill((0, 0, 0))
-    background.blit(fighter[index], (fighter_loc_x, fighter_loc_y))
+    background.blit(starfield,(0,0))
+    background.blit(fighter[spritesheet_index], (fighter_loc_x, fighter_loc_y))
     window_surface.blit(background, (0, 0))
     manager.draw_ui(window_surface)
 
--- spritesheet.py
+++ spritesheet.py
@@ -2,7 +2,8 @@ import pygame
 import json
 
 class Spritesheet:
-    def __init__(self, filename):
+    def __init__(self, name, filename):
+        self.name = name
         self.filename = filename
         self.sprite_sheet = pygame.image.load(filename).convert()
         self.meta_data = self.filename.replace('png', 'json')
@@ -16,8 +17,53 @@ class Spritesheet:
         sprite.blit(self.sprite_sheet,(0, 0),(x, y, w, h))
         return sprite
 
-    def parse_sprite(self, name):
-        sprite = self.data['frames'][name]['frame']
+    def parse_sprite(self, pose):
+        sprite = self.data['frames'][pose]['frame']
         x, y, w, h = sprite["x"], sprite["y"], sprite["w"], sprite["h"]
         image = self.get_sprite(x, y, w, h)
         return image
+
+    def frame_sheet(self):
+        sheet = [self.parse_sprite(f'{self.name}{i+1}.png') for i in range(36)]
+
+        '''  Long form of list comprehension above
+        sheet2 = [
+        self.parse_sprite(f'{self.name}1.png'),
+        self.parse_sprite(f'{self.name}2.png'),
+        self.parse_sprite(f'{self.name}3.png'),
+        self.parse_sprite(f'{self.name}4.png'),
+        self.parse_sprite(f'{self.name}5.png'),
+        self.parse_sprite(f'{self.name}6.png'),
+        self.parse_sprite(f'{self.name}7.png'),
+        self.parse_sprite(f'{self.name}8.png'),
+        self.parse_sprite(f'{self.name}9.png'),
+        self.parse_sprite(f'{self.name}10.png'),
+        self.parse_sprite(f'{self.name}11.png'),
+        self.parse_sprite(f'{self.name}12.png'),
+        self.parse_sprite(f'{self.name}13.png'),
+        self.parse_sprite(f'{self.name}14.png'),
+        self.parse_sprite(f'{self.name}15.png'),
+        self.parse_sprite(f'{self.name}16.png'),
+        self.parse_sprite(f'{self.name}17.png'),
+        self.parse_sprite(f'{self.name}18.png'),
+        self.parse_sprite(f'{self.name}19.png'),
+        self.parse_sprite(f'{self.name}20.png'),
+        self.parse_sprite(f'{self.name}21.png'),
+        self.parse_sprite(f'{self.name}22.png'),
+        self.parse_sprite(f'{self.name}23.png'),
+        self.parse_sprite(f'{self.name}24.png'),
+        self.parse_sprite(f'{self.name}25.png'),
+        self.parse_sprite(f'{self.name}26.png'),
+        self.parse_sprite(f'{self.name}27.png'),
+        self.parse_sprite(f'{self.name}28.png'),
+        self.parse_sprite(f'{self.name}29.png'),
+        self.parse_sprite(f'{self.name}30.png'),
+        self.parse_sprite(f'{self.name}31.png'),
+        self.parse_sprite(f'{self.name}32.png'),
+        self.parse_sprite(f'{self.name}33.png'),
+        self.parse_sprite(f'{self.name}34.png'),
+        self.parse_sprite(f'{self.name}35.png'),
+        self.parse_sprite(f'{self.name}36.png')
+        ]
+        '''
+        return sheet
